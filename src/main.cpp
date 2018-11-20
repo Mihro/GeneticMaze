9e96a0fc64306004a1409b2208c04d99f016994a
Mihro <officialmihro@gmail.com>
2018-11-20 23:39:31 +0000

    Extract maze dimensions

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <fstream>
+#include <iostream>
 
 int main()
 {
-	std::ifstream inFile("Labs15and16TerrainFile1.txt");
+	std::ifstream inFile("../src/Labs15and16TerrainFile1.txt");
+
+	int mazeWidth = 0, mazeHeight = 0;
+	inFile >> mazeWidth >> mazeHeight;
+	std::cout << mazeWidth << " " << mazeHeight << std::endl;
 
 	system("pause");
 	return 0;
