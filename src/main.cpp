3b19866ff827d371e5e26ec237925f6f1731d6c0
Mihro <officialmihro@gmail.com>
2018-11-21 00:14:57 +0000

    Migrated maze file data reading to function

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,39 @@
 #include <iostream>
 #include <vector>
 
+std::vector<std::vector<int>> readMazeFromFile(const char* _path);
+
 int main()
 {
-	std::ifstream inFile("../src/Labs15and16TerrainFile1.txt");
+	std::vector<std::vector<int>> mazeMap = readMazeFromFile("../src/Labs15and16TerrainFile1.txt");
 
-	int mazeWidth = 0, mazeHeight = 0;
-	inFile >> mazeWidth >> mazeHeight;
-	std::cout << mazeWidth << " " << mazeHeight << std::endl;
+	system("pause");
+	return 0;
+}
 
-	std::vector<std::vector<int>> mazeData;
-	mazeData.resize(mazeHeight, std::vector<int>(mazeWidth));
-	for (int h = 0; h < mazeHeight; h++)
+std::vector<std::vector<int>> readMazeFromFile(const char* _path)
+{
+	std::ifstream inFile(_path);
+
+	// Extract maze dimensions
+	int width = 0, height = 0;
+	inFile >> width >> height;
+	std::cout << width << " " << height << std::endl; // Debug output
+
+	// Create & resize 2D maze vector
+	std::vector<std::vector<int>> data;
+	data.resize(height, std::vector<int>(width));
+
+	// Populate 2D maze
+	for (int h = 0; h < height; h++)
 	{
-		for (int w = 0; w < mazeWidth; w++)
+		for (int w = 0; w < width; w++)
 		{
-			inFile >> mazeData[h][w];
-			std::cout << mazeData[h][w] << " ";
+			inFile >> data[h][w];
+			std::cout << data[h][w] << " "; // Debug output
 		}
 		std::cout << std::endl;
 	}
-	
-	system("pause");
-	return 0;
+
+	return data;
 }
