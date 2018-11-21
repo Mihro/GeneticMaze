0538a460d805b48dd2fff6918373f7caa0773295
Mihro <officialmihro@gmail.com>
2018-11-21 00:06:24 +0000

    Extracted data to 2D vector

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 int main()
 {
@@ -9,6 +10,18 @@ int main()
 	inFile >> mazeWidth >> mazeHeight;
 	std::cout << mazeWidth << " " << mazeHeight << std::endl;
 
+	std::vector<std::vector<int>> mazeData;
+	mazeData.resize(mazeHeight, std::vector<int>(mazeWidth));
+	for (int h = 0; h < mazeHeight; h++)
+	{
+		for (int w = 0; w < mazeWidth; w++)
+		{
+			inFile >> mazeData[h][w];
+			std::cout << mazeData[h][w] << " ";
+		}
+		std::cout << std::endl;
+	}
+	
 	system("pause");
 	return 0;
 }
